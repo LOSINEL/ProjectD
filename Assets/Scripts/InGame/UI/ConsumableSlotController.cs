969904091942ea703f354d0b993db3e3a7d26eb9
Kim Taekcheon <10cheon00@gmail.com>
2023-07-11 16:32:17 +0900

    commit for synchronization

--- Assets/Scripts/InGame/UI/ConsumableSlotController.cs
+++ Assets/Scripts/InGame/UI/ConsumableSlotController.cs
@@ -10,30 +10,19 @@ public class ConsumableSlotController : MonoBehaviour
     [SerializeField]
     private Text _consumableSlotLabel;
 
-    /// <summary>
-    /// 착용한 아이템을 인자로 받아 화면에 아이템의 이미지와 이름을 보여준다.<br/>
-    /// 
-    /// TODO<br/>
-    /// 테스트를 위해서 임의로 만든 스프라이트와 텍스트를 화면에 보여주도록 구현했다.
-    /// </summary>
-    /// <param name="item"></param>
-    public void UpdateConsumableSlot(ItemSO item = null)
+    private void Start()
     {
-        Sprite sprite = GetItemSprite(item);
-        _consumableSlotImage.sprite = sprite;
-
-        //_consumableSlotLabel.text = item.ItemName
-        _consumableSlotLabel.text = "test###";
+        _consumableSlotImage = GetComponent<Image>();
+        UpdateConsumableCountText(3, 5);
     }
 
-    /// <summary>
-    /// TODO
-    /// 테스트를 위해 하얀 스프라이트를 생성해 반환하도록 설정함.
-    /// </summary>
-    /// <param name="item"></param>
-    /// <returns></returns>
-    private Sprite GetItemSprite(ItemSO item = null)
+    public void UpdateConsumableCountText(int count = 0, int maxCount = 0)
     {
-        return Sprite.Create(Texture2D.whiteTexture, Rect.MinMaxRect(0, 0, 1, 1), Vector2.zero);
+        _consumableSlotLabel.text = $"{count}/{maxCount}";
+    }
+
+    public void ChangeConsumableSlotImage(Sprite sprite)
+    {
+        _consumableSlotImage.sprite = sprite;
     }
 }
---
+++ Assets/Scripts/InGame/UI/PlayerStatsUIController.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStatsUIController : MonoBehaviour
+{
+    [SerializeField] Text _atkValue;
+    [SerializeField] Text _healthValue;
+    [SerializeField] Text _defValue;
+    [SerializeField] Text _staminaValue;
+    [SerializeField] Text _atkSpeedValue;
+    [SerializeField] Text _moveSpeedValue;
+    [SerializeField] Text _restoreHealthValue;
+    [SerializeField] Text _restoreStaminaValue;
+    private IEnumerator _updateStatTextCoroutine;
+
+    private void OnEnable()
+    {
+        // _updateStatTextCoroutine = UpdateStatTexts();
+        // StartCoroutine(_updateStatTextCoroutine);
+    }
+
+    private void OnDisable(){
+        // StopCoroutine(_updateStatTextCoroutine);
+    }
+
+    private IEnumerator UpdateStatTexts()
+    {
+        Player.PlayerData stats = Player.instance.Data;
+        UpgradeManager upgradeStats = UpgradeManager.instance;
+        while(true){
+            _atkValue.text = $"{stats.Damage}";
+            _healthValue.text = $"{stats.MaxHp}";
+            _defValue.text = $"{stats.Defense}";
+            _staminaValue.text = $"{stats.MaxSp}";
+            _atkSpeedValue.text = $"{stats.AttackSpeed}";
+            _moveSpeedValue.text = $"{stats.MoveSpeed}";
+            _restoreHealthValue.text = $"{upgradeStats.HpRecov}";
+            _restoreStaminaValue.text = $"{Nums.staminaRecovery}";
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+}
